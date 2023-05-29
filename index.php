18abfe3507034e90cea5d293871811ae4bfc73a3
alim-2023 <alim@crystallitedigital.com>
2023-05-29 21:16:13 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Application + Game</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <header class="header">
+
+        <div class="container mainheader">
+            <div class="d-flex">
+
+                <div class="logo">
+                    <img src="assets/images/logo.svg" alt="bitswits" class="img-fluid">
+                </div>
+
+                <div class="button-header">
+                    <button class="btn btn-primary">Start a project <span class="circle"></span> </button>
+                </div>
+
+            </div>
+        </div>
+
+    </header>
+
+    <div class="show">
+        <section class="banner">
+
+            <div class="container">
+                <div class="d-flex">
+                    <h3 class="colorwhite font26 fontGM">Custom Mobile Development</h3>
+                    <h3 class="colorwhite font26 fontGM">Development</h3>
+                </div>
+                <img src="assets/images/bannertext.svg" class="img-fluid" alt="bitswits">
+                <h3 class="colorwhite font26 fontGM pt-2">WE DELIVER CUSTOM, QUALITY MOBILE APPLICATION SOLUTIONS TO
+                    BRING
+                    YOUR
+                    CONCEPTS TO MARKET.</h3>
+                <div class="row">
+                    <div class="col-md-6 mt-5">
+                        <h3 class="font30 fontGM colorwhite">Book A Call</h3>
+                        <form action="" class="formshome">
+                            <span class="font16 colorwhite fontGM">My name is</span> <input type="text" name="name"
+                                placeholder="Insert Name"> <span class="font16 colorwhite fontGM">and I'd</span>
+                            <br><br>
+                            <span class="font16 colorwhite fontGM">like to schedule a video call on</span> <select
+                                name="" id="">
+                                <option value="">schedule</option>
+                                <option value="">schedule</option>
+                                <option value="">schedule</option>
+                            </select> <span class="font16 colorwhite fontGM">at</span> <select name="" id="">
+                                <option value="">schedule</option>
+                                <option value="">schedule</option>
+                                <option value="">schedule</option>
+                            </select> <br><br> <span class="font16 colorwhite fontGM">and you can contact me on my
+                                e-mail</span> <input type="email" name="name" placeholder="insert email"> <br><br> <span
+                                class="font16 colorwhite fontGM">I live in</span> <input type="text" name="name"
+                                placeholder="Country"> <br> <br> <input type="checkbox" id="vehicle1" name="vehicle1"
+                                value="Bike"> <label for="vehicle1" class="checkpost"> agree to the terms and conditions
+                                and
+                                the privacy policy</label> <br><br>
+                            <div class="button-header">
+                                <button class="btn btn-primary">Submit <span class="circle"></span> </button>
+                            </div>
+
+
+                        </form>
+
+
+
+                    </div>
+                    <div class="col-md-6 sidenode">
+                        <img src="assets/images/bannerimage.svg" alt="bitswits" class="img-fluid sideimage">
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        <section class="star">
+
+            <div class="container uncleaer">
+
+                <div class="d-flex posting">
+                    <div class="review d-flex">
+                        <div class="people">
+                            <img src="assets/images/people.svg" class="img-fluid" alt="bitswits">
+                        </div>
+                        <div class="new_1">
+                            <img src="assets/images/star.svg" class="img-fluid" alt="bitswits">
+                            <p class="font14 colorwhite fontGM center">4.8 out of 5 (review rating) <br>
+                                Over 1,200+ reviews</p>
+                        </div>
+                    </div>
+                    <div class="high">
+
+                        <div class="data d-flex">
+                            <img src="assets/images/icon1.svg" class="img-fluid" alt="bitswits">
+                            <div class="post">
+                                <h5 class="font14 colorwhite fontGM">HIGH PERFORMER</h5>
+                                <p class="font14 colorwhite fontGM">Video Making Software</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="top">
+                        <div class="data2 d-flex">
+                            <img src="assets/images/icon2.svg" class="img-fluid" alt="bitswits">
+                            <div class="post">
+                                <h5 class="font14 colorwhite fontGM">IN TOP 3 BEST RATED</h5>
+                                <p class="font14 colorwhite fontGM">Video Making & Editing Software</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="rated">
+                    <img src="assets/images/revie.svg" alt="bitswits" class="img-fluid">
+                </div>
+
+
+
+            </div>
+
+
+        </section>
+
+        <div class="whatcan">
+            <div class="container">
+                <div class="d-flex">
+
+                    <div class="heading ">
+                        <h3 class="font71 fontGB colorwhite">What can we do <br> <span class="youtest">for you ?</span>
+                        </h3>
+                    </div>
+
+                    <div class="pare">
+                        <p class="fontGM colorwhite gray">Bitswits is a product-oriented team. We create apps that
+                            achieve your <br>
+                            business goals &hit the mark.</p>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="mvp">
+                            <h4>Develop <br> an MVP </h4>
+                            <p>We have released dozens of MVPs and full-scale products that now have millions of users
+                                worldwide.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mvp">
+                            <h4>Create <br> a design </h4>
+                            <p>We specialize in data-rich platforms, always going UX-first.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="mvp">
+                            <h4>Expand <br> your team </h4>
+                            <p>We hire only the top 1% of the recruitment funnel, to offer you the best of the best</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+
+
+
+</body>
+
+
+
+<script src="assets/js/custom.js"></script>
+
+</html>

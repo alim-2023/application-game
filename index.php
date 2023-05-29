eed71ca2e36c6175d10619d84d38b7813730ea1d
alim-2023 <alim@crystallitedigital.com>
2023-05-30 00:37:07 +0500

    shating

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
@@ -33,13 +36,13 @@
     <div class="show">
         <section class="banner">
 
-            <div class="container">
+            <div class="container mobile">
                 <div class="d-flex">
                     <h3 class="colorwhite font26 fontGM">Custom Mobile Development</h3>
                     <h3 class="colorwhite font26 fontGM">Development</h3>
                 </div>
-                <img src="assets/images/bannertext.svg" class="img-fluid" alt="bitswits">
-                <h3 class="colorwhite font26 fontGM pt-2">WE DELIVER CUSTOM, QUALITY MOBILE APPLICATION SOLUTIONS TO
+                <h2 class="application">Application + Game</h2>
+                <h3 class="colorwhite font26 fontGM">WE DELIVER CUSTOM, QUALITY MOBILE APPLICATION SOLUTIONS TO
                     BRING
                     YOUR
                     CONCEPTS TO MARKET.</h3>
@@ -47,25 +50,47 @@
                     <div class="col-md-6 mt-5">
                         <h3 class="font30 fontGM colorwhite">Book A Call</h3>
                         <form action="" class="formshome">
-                            <span class="font16 colorwhite fontGM">My name is</span> <input type="text" name="name"
-                                placeholder="Insert Name"> <span class="font16 colorwhite fontGM">and I'd</span>
-                            <br><br>
-                            <span class="font16 colorwhite fontGM">like to schedule a video call on</span> <select
-                                name="" id="">
-                                <option value="">schedule</option>
-                                <option value="">schedule</option>
-                                <option value="">schedule</option>
-                            </select> <span class="font16 colorwhite fontGM">at</span> <select name="" id="">
-                                <option value="">schedule</option>
-                                <option value="">schedule</option>
-                                <option value="">schedule</option>
-                            </select> <br><br> <span class="font16 colorwhite fontGM">and you can contact me on my
-                                e-mail</span> <input type="email" name="name" placeholder="insert email"> <br><br> <span
-                                class="font16 colorwhite fontGM">I live in</span> <input type="text" name="name"
-                                placeholder="Country"> <br> <br> <input type="checkbox" id="vehicle1" name="vehicle1"
-                                value="Bike"> <label for="vehicle1" class="checkpost"> agree to the terms and conditions
-                                and
-                                the privacy policy</label> <br><br>
+                            <div class="spacing">
+                                <span class="font16 colorwhite fontGM">My name is</span> <input type="text" name="name"
+                                    placeholder="Insert Name"> <span class="font16 colorwhite fontGM">and I'd</span>
+
+                            </div>
+                            <div class="spacing">
+                                <span class="font16 colorwhite fontGM">like to schedule a video call on</span> <select
+                                    name="" id="">
+                                    <option value="">schedule</option>
+                                    <option value="">schedule</option>
+                                    <option value="">schedule</option>
+                                </select> <span class="font16 colorwhite fontGM">at</span> <select name="" id="">
+                                    <option value="">schedule</option>
+                                    <option value="">schedule</option>
+                                    <option value="">schedule</option>
+                                </select>
+
+                            </div>
+
+                            <div class="spacing">
+
+                                <span class="font16 colorwhite fontGM">and you can
+                                    contact me on my
+                                    e-mail</span> <input type="email" name="name" placeholder="insert email">
+
+                            </div>
+
+
+                            <div class="spacing">
+
+                                <span class="font16 colorwhite fontGM">I live in</span> <input type="text" name="name"
+                                    placeholder="Country"> </div>
+
+
+                            <div class="spacing">
+                                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"> <label for="vehicle1"
+                                    class="checkpost"> agree to the
+                                    terms and conditions
+                                    and
+                                    the privacy policy</label>
+                            </div>
                             <div class="button-header">
                                 <button class="btn btn-primary">Submit <span class="circle"></span> </button>
                             </div>
@@ -132,7 +157,7 @@
 
         </section>
 
-        <div class="whatcan">
+        <section class="whatcan">
             <div class="container">
                 <div class="d-flex">
 
@@ -149,31 +174,151 @@
                 </div>
 
                 <div class="row">
-                    <div class="col-md-4">
+                    <div class="col-md-4 mobile">
                         <div class="mvp">
-                            <h4>Develop <br> an MVP </h4>
-                            <p>We have released dozens of MVPs and full-scale products that now have millions of users
+                            <h4 class="font40 fontGB colorwhite">Develop <br> an MVP </h4>
+                            <p class="gray">We have released dozens of MVPs and full-scale products that now have
+                                millions of users
                                 worldwide.</p>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 mobile">
                         <div class="mvp">
-                            <h4>Create <br> a design </h4>
-                            <p>We specialize in data-rich platforms, always going UX-first.</p>
+                            <h4 class="font40 fontGB colorwhite">Create <br> a design </h4>
+                            <p class="gray fontGM">We specialize in data-rich platforms, always going UX-first.</p>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-4 mobile">
                         <div class="mvp">
-                            <h4>Expand <br> your team </h4>
-                            <p>We hire only the top 1% of the recruitment funnel, to offer you the best of the best</p>
+                            <h4 class="font40 fontGB colorwhite">Expand <br> your team </h4>
+                            <p class="gray fontGM">We hire only the top 1% of the recruitment funnel, to offer you the
+                                best of the best</p>
                         </div>
                     </div>
                 </div>
 
             </div>
-        </div>
+        </section>
 
 
+    </div>
+
+
+    <div class="startup">
+        <section class="wework">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 mobile">
+                        <h3 class="fontinter font78 colorwhite"> <span class="work">We Work</span> With:</h3>
+                    </div>
+                </div>
+                <div class="row user">
+                    <div class="col-md-4 mobile">
+
+
+                        <div class="seed">
+                            <img src="assets/images/star1.svg" alt="" srcset="">
+                        </div>
+                        <h3 class="font23 colorwhite fontGM">Pre-Seed & <br> Seed Startups</h3>
+                        <p class="gray fontGR font14">Create a pitch deck, research your idea, run user interviews, and
+                            create MVP.</p>
+
+                    </div>
+                    <div class="col-md-4 mobile">
+                        <div class="seed">
+                            <img src="assets/images/star2.svg" alt="" srcset="">
+
+                        </div>
+                        <h3 class="font23 colorwhite fontGM">Startups <br> that Scale</h3>
+                        <p class="gray fontGR font14">Find your product-market fit, find the team
+                            with the right skill set, and scale fast without
+                            breaking things.</p>
+
+                    </div>
+                    <div class="col-md-4 mobile">
+                        <div class="seed">
+                            <img src="assets/images/star3.svg" alt="" srcset="">
+                        </div>
+                        <h3 class="font23 colorwhite fontGM">Businesses that <br> want to digitize</h3>
+                        <p class="gray fontGR font14">Redesign and modernize your platform or SaaS, and
+                            create new internal tools for better management.</p>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="ideas">
+            <div class="container">
+                <div class="d-flex">
+                    <h4 class="font78 colorwhite fontinter"> <span class="work fontweight">Startups from <br></span>
+                        ideas to ipo</h4>
+                    <p class="gray font16 fontGM comes">When it comes to startups we offer a complete range of services
+                        to help entrepreneurs succeed. From ground-zero to a booming IPO. Here’s how we do it:</p>
+                </div>
+
+                <img src="assets/images/map.svg" class="img-fluid" alt="bitwits">
+
+
+
+            </div>
+        </section>
+
+        <section class="whatcan">
+            <div class="container">
+                <div class="d-flex">
+
+                    <div class="heading ">
+                        <h3 class="font71 fontGB colorwhite"><span class="youtest">All pros.</span><br> No Cons
+                        </h3>
+                    </div>
+
+
+                </div>
+
+                <div class="row">
+                    <div class="col-md-3 mobile">
+                        <div class="mvp">
+                            <h4 class="font30 fontGB colorwhite">Outcome-focused <br> development </h4>
+                            <p class="gray">Outcome is important. Rather
+                                than just closing tickets in Jira, we
+                                try to optimize business
+                                processes, reduce expenses with
+                                just choosing right tech stack &
+                                logic.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mobile">
+                        <div class="mvp">
+                            <h4 class="font30 fontGB colorwhite">Strong <br> UX unit </h4>
+                            <p class="gray fontGM">On most projects we try to talk to
+                                users, understand their pains and
+                                wants. This helps us create the
+                                best design and solution.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mobile">
+                        <div class="mvp">
+                            <h4 class="font30 fontGB colorwhite">Regular reports <br> & demos </h4>
+                            <p class="gray fontGM">You will always know the status of
+                                the project (plan vs fact), you will
+                                have access to all documentation
+                                & can reach a team member in
+                                Slack directly.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 mobile">
+                        <div class="mvp">
+                            <h4 class="font30 fontGB colorwhite">Hiring top 1% <br> of the funnel </h4>
+                            <p class="gray fontGM">We screen 100s of candidates
+                                before selecting the best talent
+                                there is.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     </div>
 
 
